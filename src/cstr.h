abfedd9a45e791e699c356af3372e77b452316f6
friedka <78288462+lieu02tard@users.noreply.github.com>
2021-12-27 08:06:13 +0700

    Update license in source files

--- src/cstr.h
+++ src/cstr.h
@@ -1,5 +1,5 @@
 /* cstr - C string manipulation library
- * This program/library is part of the FFABC project
+ * Copyright 2021 friedka
 	This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
--- src/cstr_dbg.c
+++ src/cstr_dbg.c
@@ -1,3 +1,19 @@
+/* cstr - C string manipulation library
+ * Copyright 2021 friedka
+	This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*/
 #include "cstr_dbg.h"
 #include <stdlib.h>
 #include <stdio.h>
